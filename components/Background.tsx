f18b8d94e1c9ec1a2b240055121ad53a51981ba1
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-10-26 14:33:00 -0500

    Update Background.tsx

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,16 +1,14 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useEffect, useRef } from "react";
 
 export default function Background(props: { isMobile: boolean; link: string }) {
-  const [redirected, setRedirected] = useState(false);
-
   const router = useRouter();
 
-  function reset() {
-    setRedirected(false);
+  const video = useRef<HTMLVideoElement>(null);
 
+  function reset() {
     const title = document.getElementById("title");
     const jumpIn = document.getElementById("jump-in");
     const noggle = document.getElementById("noggle");
@@ -30,51 +28,48 @@ export default function Background(props: { isMobile: boolean; link: string }) {
   }, []);
 
   return (
-    <>
-      <video
-        muted
-        playsInline
-        preload="auto"
-        onClick={(e) => {
-          e.currentTarget.play();
-        }}
-        onEnded={(e) => {
-          setRedirected(true);
-          window.location.assign(props.link);
-          e.currentTarget.currentTime = 0;
+    <video
+      ref={video}
+      muted
+      playsInline
+      preload="auto"
+      onClick={(e) => {
+        e.currentTarget.play();
+        setTimeout(() => {
+          window.open(props.link);
           router.refresh();
-        }}
-        onTimeUpdate={(e) => {
-          if (e.currentTarget.currentTime > 0.6) {
-            const title = document.getElementById("title");
-            const jumpIn = document.getElementById("jump-in");
-            const noggle = document.getElementById("noggle");
-            const socials = document.getElementById("socials");
-
-            if (title && jumpIn && noggle && socials) {
-              title.style.display = "none";
-              jumpIn.style.display = "none";
-              noggle.style.display = "none";
-              socials.style.display = "none";
-            }
+          if (video.current) {
+            video.current.pause();
+            video.current.currentTime = 0;
           }
-        }}
-        className="cursor-pointer select-none w-full h-full object-cover object-center"
-      >
-        <source
-          src={props.isMobile ? "/background-mobile.webm" : "/background.webm"}
-          type="video/webm"
-        />
-        <source
-          src={props.isMobile ? "/background-mobile.mp4" : "/background.mp4"}
-          type="video/mp4"
-        />
-      </video>
-      {redirected ? (
-        <div className="fixed top-0 w-full h-[100svh] bg-white" />
-      ) : (
-        ""
-      )}
-    </>
+          reset();
+        }, 3200);
+      }}
+      onTimeUpdate={(e) => {
+        if (e.currentTarget.currentTime > 0.6) {
+          const title = document.getElementById("title");
+          const jumpIn = document.getElementById("jump-in");
+          const noggle = document.getElementById("noggle");
+          const socials = document.getElementById("socials");
+
+          if (title && jumpIn && noggle && socials) {
+            title.style.display = "none";
+            jumpIn.style.display = "none";
+            noggle.style.display = "none";
+            socials.style.display = "none";
+          }
+        }
+      }}
+      className="cursor-pointer select-none w-full h-full object-cover object-center"
+    >
+      <source
+        src={props.isMobile ? "/background-mobile.webm" : "/background.webm"}
+        type="video/webm"
+      />
+      <source
+        src={props.isMobile ? "/background-mobile.mp4" : "/background.mp4"}
+        type="video/mp4"
+      />
+    </video>
   );
 }
