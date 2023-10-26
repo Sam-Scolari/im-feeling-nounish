cffe9c4bb5f85c7555e5f824fff687307ca1de1e
Sam Scolari <56174573+Sam-Scolari@users.noreply.github.com>
2023-10-26 14:38:10 -0500

    Update Background.tsx

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { useEffect, useRef } from "react";
+import { useEffect, useState } from "react";
 
 export default function Background(props: { isMobile: boolean; link: string }) {
+  const [redirected, setRedirected] = useState(false);
+
   const router = useRouter();
 
-  const video = useRef<HTMLVideoElement>(null);
-
   function reset() {
+    setRedirected(false);
+
     const title = document.getElementById("title");
     const jumpIn = document.getElementById("jump-in");
     const noggle = document.getElementById("noggle");
@@ -28,48 +30,52 @@ export default function Background(props: { isMobile: boolean; link: string }) {
   }, []);
 
   return (
-    <video
-      ref={video}
-      muted
-      playsInline
-      preload="auto"
-      onClick={(e) => {
-        e.currentTarget.play();
-        setTimeout(() => {
-          window.open(props.link);
-          router.refresh();
-          if (video.current) {
-            video.current.pause();
-            video.current.currentTime = 0;
-          }
-          reset();
-        }, 3200);
-      }}
-      onTimeUpdate={(e) => {
-        if (e.currentTarget.currentTime > 0.6) {
-          const title = document.getElementById("title");
-          const jumpIn = document.getElementById("jump-in");
-          const noggle = document.getElementById("noggle");
-          const socials = document.getElementById("socials");
+    <>
+      <video
+        muted
+        playsInline
+        preload="auto"
+        onClick={(e) => {
+          e.currentTarget.play();
+        }}
+        onEnded={(e) => {
+          setRedirected(true);
+          e.currentTarget.currentTime = 0;
+          window.location.href = props.link;
 
-          if (title && jumpIn && noggle && socials) {
-            title.style.display = "none";
-            jumpIn.style.display = "none";
-            noggle.style.display = "none";
-            socials.style.display = "none";
+          // router.refresh();
+        }}
+        onTimeUpdate={(e) => {
+          if (e.currentTarget.currentTime > 0.6) {
+            const title = document.getElementById("title");
+            const jumpIn = document.getElementById("jump-in");
+            const noggle = document.getElementById("noggle");
+            const socials = document.getElementById("socials");
+
+            if (title && jumpIn && noggle && socials) {
+              title.style.display = "none";
+              jumpIn.style.display = "none";
+              noggle.style.display = "none";
+              socials.style.display = "none";
+            }
           }
-        }
-      }}
-      className="cursor-pointer select-none w-full h-full object-cover object-center"
-    >
-      <source
-        src={props.isMobile ? "/background-mobile.webm" : "/background.webm"}
-        type="video/webm"
-      />
-      <source
-        src={props.isMobile ? "/background-mobile.mp4" : "/background.mp4"}
-        type="video/mp4"
-      />
-    </video>
+        }}
+        className="cursor-pointer select-none w-full h-full object-cover object-center"
+      >
+        <source
+          src={props.isMobile ? "/background-mobile.webm" : "/background.webm"}
+          type="video/webm"
+        />
+        <source
+          src={props.isMobile ? "/background-mobile.mp4" : "/background.mp4"}
+          type="video/mp4"
+        />
+      </video>
+      {redirected ? (
+        <div className="fixed top-0 w-full h-[100svh] bg-white" />
+      ) : (
+        ""
+      )}
+    </>
   );
 }
