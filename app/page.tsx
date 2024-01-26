7b05f976db7d9e13060b56290bf931417636f28e
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-01-26 15:24:24 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Background from "@/components/Background";
 import fetchLink from "@/utils/fetchLink";
 import { headers } from "next/headers";
+import { Head } from "next/head";
 
 export default async function Home() {
   const isMobile = headers().get("x-device-type") === "mobile";
@@ -9,6 +10,10 @@ export default async function Home() {
 
   return (
     <main className="relative bg-green h-[100svh] flex items-center justify-center">
+      <Head>
+<meta property="fc:frame" content="vNext" />
+<meta property="fc:frame:image" content="http://...image-question.png" />
+<meta property="fc:frame:button:1" content="I'm Feeling Nounish" /></Head>
       <Background isMobile={isMobile} link={link} />
       <h1
         id="title"
