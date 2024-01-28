b9e38f6a3f66dfe3ceaf0867660406aad928bf6b
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-01-27 20:19:41 -0600

    .

--- app/frame/route.ts
+++ app/frame/route.ts
@@ -5,10 +5,13 @@ export async function GET(request: NextRequest) {
     ` <!DOCTYPE html>
     <html>
       <head>
-      <meta property="fc:frame" content="vNext" />
-      <meta property="fc:frame:image" content="https://nouns.ooo/og.png" />
-      <meta property="fc:frame:button:1" content="I'm Feeling Nounish" />
-      <meta property="fc:frame:post_url" content="https://nouns.ooo">
+        <meta property="og:title" content="I'm Feeling Nounish" />
+        <meta property="og:description" content="Discover new and upcoming nounish people, places, and things!" />
+        <meta property="og:image" content="https://nouns.ooo/og.png" />
+        <meta property="fc:frame" content="vNext" />
+        <meta property="fc:frame:image" content="https://nouns.ooo/og.png" />
+        <meta property="fc:frame:button:1" content="I'm Feeling Nounish" />
+        <meta property="fc:frame:post_url" content="https://nouns.ooo">
       </head>
     </html>
         
