26327c223a1872f2a347b0000293ce4daddb5620
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-01-27 20:02:17 -0600

    .

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,22 @@ import Background from "@/components/Background";
 import fetchLink from "@/utils/fetchLink";
 import { headers } from "next/headers";
 import Head from "next/head";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  return new NextResponse(
+    `
+        <!DOCTYPE html>
+        <html>
+          <head>
+                    <meta property="fc:frame" content="vNext" />
+                    <meta property="fc:frame:image" content="/og.png" />
+          </head>
+        </html>
+        `,
+    { status: 200, headers: { "content-type": "text/html" } }
+  );
+}
 
 export default async function Home() {
   const isMobile = headers().get("x-device-type") === "mobile";
