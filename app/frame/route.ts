05522f3f7d0fb490b12b22af359d99f779d4c8e9
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-01-28 12:38:12 -0600

    POST

--- app/frame/route.ts
+++ app/frame/route.ts
@@ -1,4 +1,7 @@
+import fetchLink from "@/utils/fetchLink";
 import { NextRequest, NextResponse } from "next/server";
+import ogs from "open-graph-scraper";
+import sharp from "sharp";
 
 export async function GET(request: NextRequest) {
   return new NextResponse(
@@ -21,13 +24,33 @@ export async function GET(request: NextRequest) {
 }
 
 export async function POST(request: NextRequest) {
+  const { result } = await ogs({ url: await fetchLink() });
+
+  const { ogTitle, ogDescription, ogImage } = result;
+
+  const imageURL = ogImage?.[0].url ?? "";
+
+  const image = await fetch(imageURL);
+
+  const resize = (await sharp(await image.arrayBuffer())).resize(
+    191 * 5,
+    100 * 5
+  );
+
+  const base64 =
+    "data:image/png;base64," + (await resize.toBuffer()).toString("base64");
+
   return new NextResponse(
     `
         <!DOCTYPE html>
         <html>
           <head>
+                <meta property="og:title" content=${ogTitle} />
+                <meta property="og:description" content=${ogDescription} />
+                <meta property="og:image" content="${ogImage?.[0].url}" />
                 <meta property="fc:frame" content="vNext" />
-                <meta property="fc:frame:image" content="https://nouns.ooo/placeholder.webp" />
+                <meta property="fc:frame:image" content="${base64}" />
+                <meta property="fc:frame:button:1" content="Explore →" />
           </head>
         </html>
         `,
