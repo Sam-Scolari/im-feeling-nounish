8a08c5f36eae3c0cfacba7f0f6d6d37750cc2e05
Sam ⌐◨-◨ <56174573+Sam-Scolari@users.noreply.github.com>
2024-01-30 17:52:51 -0600

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -17,5 +17,5 @@ export function middleware(request: NextRequest) {
 
 // only apply this middleware to files in the app directory
 export const config = {
-  matcher: "/((?!api|static|.*\\..*|_next).*)",
+  matcher: "/((?!api|static|redirects|frame|.*\\..*|_next).*)",
 };
